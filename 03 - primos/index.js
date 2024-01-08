46f8598c0ecacae1a91f2f567421495b46f4f296
Daniel Martínez <danielmartinez.damar@hotmail.com>
2024-01-08 17:40:37 -0600

    add 03 solution

---
+++ 03 - primos/index.js
@@ -0,0 +1,24 @@
+/*
+ * Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ */
+
+function primo (num) {
+  if (num <= 1) {
+    return false
+  } 
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false
+    }
+  }
+
+  return true
+}
+
+for (let i = 1; i <= 100; i ++) {
+  if (primo(i)) {
+    console.log(i)
+  }
+}
